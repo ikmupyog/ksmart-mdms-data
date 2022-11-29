7ca3ceb185f351f6e84677ccf596f04bfb6886d3
Lekshmy B L <94520731+lekshmybl@users.noreply.github.com>
2022-11-29 14:08:21 +0530

    Create religion.json

---
+++ data/kl/common-masters/Religion.js
@@ -0,0 +1,27 @@
+{
+  "tenantId": "kl",
+  "moduleName": "birth-death-service",
+  "Religion": [
+    {
+      "name": "Hindu",
+      "namelocal": "",
+      "code": "RELIGION_HINDU",
+      "active": true,
+      "type": "BND"
+    },
+    {
+      "name": "Muslim",
+      "namelocal": "",
+      "code": "RELIGION_MUSLIM",
+      "active": true,
+      "type": "BND"
+    },
+    {
+      "name": "Christian",
+      "namelocal": "",
+      "code": "RELIGION_CHRISTIAN",
+      "active": true,
+      "type": "BND"
+    }
+  ]
+}
